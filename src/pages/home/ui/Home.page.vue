b948a0bee14e19741d06f6a77c3051c905f9cd3b
Nikita <nikitalomieiko@gmail.com>
2025-04-05 04:56:15 +0300

    feat send box

--- src/pages/home/ui/Home.page.vue
+++ src/pages/home/ui/Home.page.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Message } from "entities/message";
+import {SendBox} from "features/send-box";
 </script>
 
 <template>
@@ -14,16 +15,19 @@ import { Message } from "entities/message";
       body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eius tenetur ipsa molestiae? Ducimus laboriosam, quidem nisi dolores eius modi. Maiores nostrum omnis distinctio necessitatibus, illum quo culpa sit nemo.
 "
     />
-    <Message
-      v-on:delete="() => null"
-      v-on:bookmark="() => null"
-      v-on:answer="() => null"
-      orientation="right"
-      type-author="user"
-      time="14:21"
-      body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eius tenetur ipsa molestiae? Ducimus laboriosam, quidem nisi dolores eius modi. Maiores nostrum omnis distinctio necessitatibus, illum quo culpa sit nemo.
-"
-    />
+    <div class="w-full flex justify-end">
+        <Message
+            class=""
+          v-on:delete="() => null"
+          v-on:bookmark="() => null"
+          v-on:answer="() => null"
+          orientation="right"
+          type-author="user"
+          time="14:21"
+          body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eius tenetur ipsa molestiae? Ducimus laboriosam, quidem nisi dolores eius modi. Maiores nostrum omnis distinctio necessitatibus, illum quo culpa sit nemo.
+    "
+        />
+    </div>
     <Message
       v-on:delete="() => null"
       v-on:bookmark="() => null"
@@ -34,6 +38,8 @@ import { Message } from "entities/message";
       body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eius tenetur ipsa molestiae? Ducimus laboriosam, quidem nisi dolores eius modi. Maiores nostrum omnis distinctio necessitatibus, illum quo culpa sit nemo.
 "
     />
+    <SendBox message="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo nam commodi aspernatur perspiciatis officia sit quisquam distinctio? Quod, ex! Deserunt beatae ex dolorum quis a enim vel possimus aliquid! Molestiae!
+"/>
   </div>
 </template>
 
